0effa8d1c83889add6605e7b72346c659e56d788
Matt Wiley <wileymab@gmail.com>
2019-06-20 23:49:27 -0400

    Simple change. Added comments to test the Tampermonkey update check feature.

--- google/no-ads-google-search.js
+++ google/no-ads-google-search.js
@@ -10,12 +10,19 @@
 
 (function() {
     'use strict';
+    /**
+     * Given a list of selectors, will add hidden attributes to the respective 
+     * elements to hide the from view.
+     * 
+     * @param {*} selectors array of CSS selectors to be hidden
+     */
     const hide = function(selectors) {
         for(let i = 0; i < selectors.length; i++) {
             document.querySelector(selectors[i]).setAttribute("hidden","hidden")
         }
     }
 
+    // Hide these elements.
     hide([
         "#taw",
         ".iq-content-first"
